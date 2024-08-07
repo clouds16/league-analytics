fed8f365faa58921de089831ab2afa27269af2e8
Hector Alvarez <hect16@gmail.com>
2024-08-07 11:17:38 -0600

    added basic layout for pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,44 +1,22 @@
 import React from "react";
-
-
+import Footer from './common/Footer'
+import Header from './common/Header'
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { headerLinks, footerLinks } from "./utils";
 
-const Layout = ({ children, ...otherProps }) => {
+const Layout = ({ children }) => {
     return (
-
-        <Container {...otherProps}>
-            <Container>
-                <Navbar expand="lg" className="bg-body-tertiary">
-                    <Container>
-                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#link">Link</Nav.Link>
-                                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">
-                                        Another action
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.4">
-                                        Separated link
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </Container>
+        <>
+            <Header title="League Analytics" links={headerLinks} />
             <Container>
                 {children}
             </Container>
-        </Container>
+            <Footer
+                copyrightYear={new Date().getFullYear()}
+                companyName="League Analytics Inc."
+                links={footerLinks}
+            />
+        </>
     );
 }
 
